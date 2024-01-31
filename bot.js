98dea376b2491db3f520c740c83d2ad0844b68a8
Jason Pien <jasonpien@jasonsmbp16096.devices.brown.edu>
2024-01-30 22:07:09 -0500

    log in

--- bot.js
+++ bot.js
@@ -1,20 +1,8 @@
 const puppeteer = require("puppeteer");
 
-const target = 'https://my.lifetime.life/clubs/nj/berkeley-heights/resource-booking.html?sport=Pickleball%3A+Indoor&clubId=188&date=2024-01-31&startTime=-1&duration=60&hideModal=true'
+const target = 'https://my.lifetime.life/clubs/nj/berkeley-heights/resource-booking.html?sport=Pickleball%3A+Indoor&clubId=188&date=2024-01-31&startTime=-1&duration=60&hideModal=true';
 
-async function initBrowser() {
-    try {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.goto(target, { waitUntil: 'networkidle2' });
-        return page;
-    } catch (error) {
-        console.error("Error in initBrowser:", error);
-        throw error;
-    }
-}
-
-async function logIn(page) {
+async function ClicklogIn(page) {
     await page.$eval('span.ico-user.icon-adjust-horiz', elem => elem.click());
     await page.evaluate(() => {
         const loginLink = document.querySelector('a.js-login-link.btn.btn-primary.btn-sm.btn-block');
@@ -22,13 +10,39 @@ async function logIn(page) {
             loginLink.click();
         }
     });
+}
 
+async function fillUsername(page) {
+    const usernameSelector = '#account-username'; 
+    await page.waitForSelector(usernameSelector); 
+    await page.type(usernameSelector, 'berkeleygirl');  
+}
+
+async function fillPassword(page) {
+    const usernameSelector = '#account-password';
+    await page.waitForSelector(usernameSelector); 
+    await page.type(usernameSelector, 'JanJu1328'); 
+}
+
+async function clickButton(page) {
+    const buttonSelector = '.btn-icon-text'; 
+    await page.waitForSelector(buttonSelector, { visible: true });
+    await page.click(buttonSelector); 
 }
 
 
-async function checkout() {
-    const page = await initBrowser();
-    await logIn(page)
+async function main() {
+    try {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto(target, { waitUntil: 'networkidle2' });
+        await ClicklogIn(page);
+        await fillUsername(page);
+        await fillPassword(page);
+        await clickButton(page);
+    } catch (error) {
+        console.error('Error in main:', error);
+    }
 }
 
-checkout().catch(err => console.error(err));
+main();
